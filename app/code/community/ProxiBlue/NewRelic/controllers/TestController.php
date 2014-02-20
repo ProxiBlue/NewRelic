8a5eb9909160b17a870a0fdf274fdb7f6ca43ccd
Lucas van Staden <lucas@dhemdia.com.au>
2014-02-20 11:51:53 +0800

    Add LICENCE and headers to each file

--- app/code/community/ProxiBlue/NewRelic/controllers/TestController.php
+++ app/code/community/ProxiBlue/NewRelic/controllers/TestController.php
@@ -1,3 +1,22 @@
+<?php       
+  /**
+  *    This file is part of ProxiBlue NewRelic Module  available via GitHub https://github.com/ProxiBlue/NewRelic     
+  *
+  *    ProxiBlue NewRelic Module is free software: you can redistribute it and/or modify
+  *    it under the terms of the GNU General Public License as published by
+  *    the Free Software Foundation, either version 3 of the License, or
+  *    (at your option) any later version.
+  *
+  *    ProxiBlue NewRelic Module is distributed in the hope that it will be useful,
+  *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+  *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  *    GNU General Public License for more details.
+  *
+  *    You should have received a copy of the GNU General Public License
+  *    along with ProxiBlue NewRelic Module.  
+  *    If not, see <http://www.gnu.org/licenses/>.
+  **/
+?>
 <?php
 
 class ProxiBlue_NewRelic_TestController extends Mage_Core_Controller_Front_Action {
